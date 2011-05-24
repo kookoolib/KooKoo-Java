db75b55d300d08bd08b6f98c3395fbda173dab9d
KooKoo <support@kookoo.in>
2011-05-24 18:32:51 +0530

    Added Dial class and addDial function to Response to make outbound Dials.

---
+++ com/ozonetel/kookoo/Dial.java
@@ -0,0 +1,84 @@
+ package com.ozonetel.kookoo;
+ 
+ import java.io.StringWriter;
+ import javax.xml.parsers.DocumentBuilder;
+ import javax.xml.parsers.DocumentBuilderFactory;
+ import javax.xml.transform.Transformer;
+ import javax.xml.transform.TransformerFactory;
+ import javax.xml.transform.dom.DOMSource;
+ import javax.xml.transform.stream.StreamResult;
+ import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+ 
+ public class Dial
+ {
+   DocumentBuilderFactory dbfac;
+   DocumentBuilder docBuilder;
+   Document doc;
+   Element dial;
+ 
+   public Dial()
+   {
+     try
+     {
+       this.dbfac = DocumentBuilderFactory.newInstance();
+      this.docBuilder = this.dbfac.newDocumentBuilder();
+      this.doc = this.docBuilder.newDocument();
+ 
+       this.dial = this.doc.createElement("dial");
+       this.doc.appendChild(this.dial);
+     }
+     catch (Exception e) {
+       e.printStackTrace();
+     }
+   }
+ 
+   public Dial(Boolean record)
+   {
+     try
+     {
+       this.dbfac = DocumentBuilderFactory.newInstance();
+       this.docBuilder = this.dbfac.newDocumentBuilder();
+       this.doc = this.docBuilder.newDocument();
+ 
+       this.dial = this.doc.createElement("dial");
+       this.dial.setAttribute("record", record.toString());
+       this.doc.appendChild(this.dial);
+     }
+     catch (Exception e) {
+       e.printStackTrace();
+     }
+   }
+ 
+ 
+ 
+   public void setRecord(Boolean record) {
+     this.dial.setAttribute("record", record.toString());
+   }
+ 
+
+   public String getXML()
+   {
+     String xmlString = "";
+     try {
+       TransformerFactory transfac = TransformerFactory.newInstance();
+       Transformer trans = transfac.newTransformer();
+       trans.setOutputProperty("omit-xml-declaration", "yes");
+       trans.setOutputProperty("indent", "yes");
+ 
+       StringWriter sw = new StringWriter();
+       StreamResult result = new StreamResult(sw);
+       DOMSource source = new DOMSource(this.doc);
+       trans.transform(source, result);
+       xmlString = sw.toString();
+     }
+     catch (Exception e) {
+       e.printStackTrace();
+     }
+     return xmlString;
+   }
+ 
+   public Element getRoot() {
+     return this.dial;
+   }
+ }
