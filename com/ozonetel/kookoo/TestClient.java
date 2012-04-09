69f783dfc6472aaa9567eed04504c7fbf4f57f7a
pavanvusirika <pavanvusirika@gmail.com>
2012-04-09 14:52:34 +0530

    Added sms feature.

--- com/ozonetel/kookoo/TestClient.java
+++ com/ozonetel/kookoo/TestClient.java
@@ -4,15 +4,7 @@ public class TestClient {
 
     public static void main(String[] args) {
         Response resp = new Response();
-
-        Dial dial= new Dial();
-        dial.setNumber("99999999999");
-        dial.setMusicOnHold(Dial.MusicOnHold.NORMAL);
-//        dial.setMusicOnHold(Dial.MusicOnHold.DEFAULT);
-//        dial.setMusicOnHold(Dial.MusicOnHold.RING);
-        resp.addDial(dial);
-
-       
+        resp.sendSms("Hi KooKoo", "9490607378");
         System.out.println(""+resp.getXML());
     }
 }
